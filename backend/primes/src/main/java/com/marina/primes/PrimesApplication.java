802c53041350c849fe146db2bc7f42f10cce3e30
Marina Benvenuti <marinabcardeal@gmail.com>
2024-04-21 15:22:52 -0300

    criação do backend

---
+++ backend/primes/src/main/java/com/marina/primes/PrimesApplication.java
@@ -0,0 +1,13 @@
+package com.marina.primes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimesApplication.class, args);
+	}
+
+}

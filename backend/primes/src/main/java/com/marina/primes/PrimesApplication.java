21a432902edc7292dd204bf1a3237fb87bf649ce
Marina Benvenuti <marinabcardeal@gmail.com>
2024-04-21 23:49:43 -0300

    corrigindo o cors

--- backend/primes/src/main/java/com/marina/primes/PrimesApplication.java
+++ backend/primes/src/main/java/com/marina/primes/PrimesApplication.java
@@ -6,14 +6,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import java.util.ArrayList;
 
+
+@CrossOrigin(origins = "http://localhost:3000")
 @SpringBootApplication
 @RestController
 public class PrimesApplication
 {
-
 	public static void main(String[] args) {
 		SpringApplication.run(PrimesApplication.class, args);
 	}

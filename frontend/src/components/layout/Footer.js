1608e58f5a1c45e3d4f0719559c617d89f08ff3c
Marina Benvenuti <marinabcardeal@gmail.com>
2024-04-23 11:29:14 -0300

    link no footer

--- frontend/src/components/layout/Footer.js
+++ frontend/src/components/layout/Footer.js
@@ -4,7 +4,8 @@ import './Footer.css';
 const Footer = () => {
     return (
       <footer>
-        <p className="mb-1">&copy; 2024 Marina B Web development</p>
+        <p className="mb-1">&copy; 2024 
+        <a href="https://github.com/marinabenvenuti" target='_blank' rel='noreferrer'> Marina B Web development</a></p>
       </footer>
     );
   };

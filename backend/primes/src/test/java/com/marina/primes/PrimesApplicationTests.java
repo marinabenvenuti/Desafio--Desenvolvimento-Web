49824d0a70ff73417efb7a268b35af9da0f7a0b5
Marina Benvenuti <marinabcardeal@gmail.com>
2024-04-22 22:57:55 -0300

    testes unitários no backend

--- backend/primes/src/test/java/com/marina/primes/PrimesApplicationTests.java
+++ backend/primes/src/test/java/com/marina/primes/PrimesApplicationTests.java
@@ -1,13 +1,29 @@
 package com.marina.primes;
 
+import com.marina.primes.DTO.Primes;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class PrimesApplicationTests {
 
+	@Autowired
+	private PrimesApplication primesApplication;
+
 	@Test
-	void contextLoads() {
+	void receiveCorrectValueForValidInput() {
+		Primes result = primesApplication.amountOfPrimeNumbersBeforeK(100L);
+
+		assertThat(result.getAmountOfPrimeNumbers()).isEqualTo(25L);
+	}
+
+	void receiveZeroForNegativeNumbers() {
+		Primes result = primesApplication.amountOfPrimeNumbersBeforeK(-100L);
+
+		assertThat(result.getAmountOfPrimeNumbers()).isEqualTo(0);
 	}
 
 }
